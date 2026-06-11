842c79e36d2dd344c36903c4a131d6be62d97e1e
adityapavan@tetrisinnovations.com <adityapavangollapudi@gmail.com>
2026-06-11 06:28:36 +0530

    Added dummy todo data

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
+let todos = [
+  {
+    id: 1,
+    title: "Learn Express",
+    completed: false,
+  },
+];
 app.listen(3000,()=>{
     console.log("server is running happily!!")
 });
