f2fdb0d280613f064bbabe12948b6085a25c4f74
Adithya Pavan Gollapudi <adityapavangollapudi@gmail.com>
2026-06-14 14:34:11 +0530

    Modifications for the apis for data flow

---
+++ controllers/todoController.js
@@ -0,0 +1,7 @@
+let todos = [
+    {
+        id: 1,
+        title: "Learn Express",
+        completed: false,
+    },
+];
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }))
 let todos = [
     {
         id: 1,
